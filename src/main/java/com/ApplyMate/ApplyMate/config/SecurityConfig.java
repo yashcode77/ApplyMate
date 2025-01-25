8d722c1e145c96240dd86cb829bc7612e387a49b
yashchhaproo <yashchhaproo@gmail.com>
2025-01-26 00:45:52 +0530

    update security config

--- src/main/java/com/ApplyMate/ApplyMate/config/SecurityConfig.java
+++ src/main/java/com/ApplyMate/ApplyMate/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
